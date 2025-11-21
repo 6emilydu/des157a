dabef2cbdc0a7cfacb4e614d0b1d788a24909f06
Emily <6emilydu@gmail.com>
2025-11-20 22:53:49 -0800

    popup

--- everypicture/v2-final/script.js
+++ everypicture/v2-final/script.js
@@ -50,7 +50,7 @@
 
       // positioning popup
     popup.style.position = 'fixed';
-    popup.style.top = '53%';
+    popup.style.top = '55%';
     popup.style.left = '50%';
     popup.style.transform = 'translate(-50%, -50%)';
     }
